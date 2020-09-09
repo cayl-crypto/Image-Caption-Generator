cc97343f7091c589ba57c64ed2d0abcb13e09504
Özkan Çaylı <caylio64@gmail.com>
2020-09-10 00:59:13 +0300

    Feature extraction is completed.

--- image_caption_generator.py
+++ image_caption_generator.py
@@ -2,7 +2,7 @@ import torch
 import torchvision
 import torch.nn as nn
 from torchvision.models import inception_v3
-
+torch.set_default_tensor_type('torch.cuda.FloatTensor')
 import numpy as np
 from utils import load_mscoco_annotations_val, show_image, load_image, gray_to_RGB
 from create_vocabulary import Voc, normalizeAllCaptions
@@ -46,37 +46,56 @@ val_captions, val_image_names = load_mscoco_annotations_val()
 
 # DONE show images
 
-# TODO extract image features.
-
-
-#encoder = Encoder()
+# DONE extract image features.
+encoder = inception_v3(pretrained=True)
+encoder.eval()
+encoder.cuda()
 preprocess = transforms.Compose([
             transforms.Resize(299),
             transforms.CenterCrop(299),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
-all_images = []
-for im in tqdm(val_image_names):
-    im = load_image(im)
-    im = gray_to_RGB(im)
-    #im = preprocess(im)
-    all_images.append(im)
-
-all_images = np.array(all_images)
-#image = load_image(val_image_names[0])
-inception = inception_v3(pretrained=True)
-inception.eval()
+feature_size = 2048
 
 
-#input_tensor = preprocess(image)
-#input_batch = input_tensor.unsqueeze(0) 
-encoder = inception_v3(pretrained=True)
-encoder.eval()
-o = encoder.forward(all_images)
-print(o.shape)
+#all_image_features = torch.zeros(len(val_image_names), feature_size)
+#feature_extraction_batch_size = 100
+#image_batch = torch.zeros(feature_extraction_batch_size, 3, 299, 299)
+#start_ids_of_batch = 0
+#end_ids_of_batch = start_ids_of_batch + feature_extraction_batch_size
+
+#def batch_extractor(model, batch):
+#    with torch.no_grad():
+#        features = model.forward(batch)
+#    return features
 
 # Extract all features with batches
+#for ids, im_path in tqdm(enumerate(val_image_names)):
+#    # completes in about 30 mins.
+#    im = load_image(im_path)
+#    im = gray_to_RGB(im)
+#    im = preprocess(im)
+#    batch_index = ids % feature_extraction_batch_size
+#    image_batch[batch_index] = im
+
+#    if ids == len(val_image_names) - 1:
+#        batch_size = ids % 100     
+#        image_batch_features = batch_extractor(encoder, image_batch[:batch_size+1])
+#        all_image_features[start_ids_of_batch:ids+1] = image_batch_features
+#        break
+
+#    if batch_index == feature_extraction_batch_size - 1:   
+#        image_batch_features = batch_extractor(encoder, image_batch)
+#        all_image_features[start_ids_of_batch:end_ids_of_batch] = image_batch_features
+#        start_ids_of_batch = end_ids_of_batch
+#        end_ids_of_batch = start_ids_of_batch + feature_extraction_batch_size
+        
+
+#np.save('val_image_features.npy', all_image_features)
+#torch.save(all_image_features, 'val_image_features.pt')
+val_features = torch.load("val_image_features.pt")
+
 
 
 
